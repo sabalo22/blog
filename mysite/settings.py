035bae5cd3697fae552ccd660839d55f88455dc1
Orlando <sabalo22@gmail.com>
2018-05-30 22:48:01 -0700

    Updated settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '13e&zc5kgi$e)hiq0)izme1wl#*hfn2%a&_q9fd@waw8%cs$qd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
